e8ab9acd36c624e10cc900b6ce8b42633010c93e
AnnaPetrosian <annapetrosian9@gmail.com>
2018-11-20 15:33:31 +0100

    Iets

---
+++ vierdeweek/ToegangsP.java
@@ -0,0 +1,46 @@
+class Demo{
+	public class OefeningObjectReturn {
+	    public static void main(String[] args) {
+	        System.out.println("Welkom");
+	        ToegangsPoort tp = new ToegangsPoort();
+	        Persoon persoon = new Persoon();
+	        persoon.naam = "Henk";
+	        persoon.pasje.geblokkeerd = true;
+	        tp.toelaten(persoon);
+	        Beveiliger Jan = new Beveiliger();
+	        persoon.pasje = Jan.pasjeUitdelen(persoon);
+	        tp.toelaten(persoon);
+	    }
+	}
+
+	class ToegangsPoort {
+	    void toelaten(Persoon dePersoon) {
+	        if (dePersoon.pasje.geblokkeerd){
+	            System.out.println("Je bent niet toegelaten");
+	        }else {
+	            System.out.println("Je bent toegelaten, welkom " + dePersoon.naam);
+	        }
+	    }
+	}
+
+	class Persoon {
+	    String naam;
+	    Pasje pasje = new Pasje();
+	}
+
+	class Pasje {
+	    boolean geblokkeerd = false;
+	}
+
+	class Beveiliger {
+	    Pasje pasjeUitdelen(Persoon persoon) {
+	        if (persoon.pasje.geblokkeerd) {
+	            System.out.println("Even de gegevens checken");
+	            return new Pasje();
+	        }else{
+	            System.out.println("Val me niet lastig");
+	            return persoon.pasje;
+	        }
+	    }
+	}
+	© 2018 GitHub, Inc.
---
+++ vierdeweek/Verzinopdracht.java
@@ -0,0 +1,35 @@
+package vierdeweek;
+
+public class Verzinopdracht {
+
+	public static void main(String[] args) {
+		Winkel winkel = new Winkel();
+		Persoon persoon = new Persoon();
+		persoon.naam= "Pietje";
+		winkel.boodschappenmaken(persoon);
+		Geld geld = new Geld();
+		
+}
+
+}
+class Winkel{
+	void boodschappenmaken(Persoon mPersoon) {
+		if(mPersoon.geld.geengeld) {
+			System.out.println("Geen geld!!! Geen boodschappen");
+		}else
+			System.out.println("Welkom: Wat wilt u hebben!!!");
+		
+	}
+
+}
+		
+class Persoon{
+	String naam;
+	Geld geld = new Geld();
+	
+}
+class Geld{
+	
+	boolean geengeld = false;
+}
+	
